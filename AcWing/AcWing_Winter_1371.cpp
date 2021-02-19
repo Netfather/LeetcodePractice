644f5a2c8362a026023cb1198e10f5304bbcab7b
汪家喆 <ahhssxlcwjz@outlook.com>
2021-02-19 20:07:47 +0900

    完全背包问题，dp问题。非常重要。需要完全理解状态的定义。任意在V种货币种选择，使得结果为Target

--- AcWing/AcWing_Winter_1371.cpp
+++ AcWing/AcWing_Winter_1371.cpp
@@ -18,45 +18,57 @@
 #include <climits>
 using namespace std;
 
-const int V = 25;
-int Bi[V];
-int cnt = 0;
-long long sum = 0;
+//const int V = 25;
+//int Bi[V];
+//int cnt = 0;
+//long long sum = 0;
+//
+////此法雖然結果正確 但是超時
+////使用暴力搜索是不適合的。
+//void dfs(int target,int v,int start)
+//{
+//	//终止条件判断区
+//	if (sum >= target)
+//	{
+//		if (sum == target) cnt++;
+//		return;
+//	}
+//
+//	for (int i = start; i < v; i++)
+//	{
+//		if (sum + Bi[i] <= target)
+//		{
+//			sum += Bi[i];
+//			dfs(target, v, i);
+//			sum -= Bi[i]; //恢复现场
+//		}
+//		else return;
+//	}
+//}
 
-void dfs(int target,int v,int start)
-{
-	//终止条件判断区
-	if (sum >= target)
-	{
-		if (sum == target) cnt++;
-		return;
-	}
+//使用dp來解題
+const int V = 30;
+const int N = 10010;
 
-	for (int i = start; i < v; i++)
-	{
-		if (sum + Bi[i] <= target)
-		{
-			sum += Bi[i];
-			dfs(target, v, i);
-			sum -= Bi[i]; //恢复现场
-		}
-		else return;
-	}
-}
+long long dp[V][N];
+//這個dp表示 從1到第i個貨幣，任意選擇使得加和為N的方案有多少種？
 
 int main()
 {
 	int v,n;
 	cin >> v >> n;
-	for (int i = 0; i < v; i++)
+	dp[0][0] = 1;//這一種方案表示什麼都不選
+	for (int i = 1; i <= v; i++)
 	{
-		cin >> Bi[i];
+		int v;
+		cin >> v;
+		for (int j = 0; j <= n; j++)
+		{
+			dp[i][j] = dp[i - 1][j]; //先把不包括當前輸入的數據，使得加和為j的數據加入
+			//然後判斷，如果當前輸入的數據比j小，說明當前的數據可以累加，此時再算上本次的加和
+			if (j >= v) dp[i][j] += dp[i][j - v];
+		}
 	}
-	sort(Bi, Bi + v);
-
-	dfs(n,v,0);
-
-	cout << cnt << endl;
-
+	cout << dp[v][n] << endl;
 	return 0;
 }
