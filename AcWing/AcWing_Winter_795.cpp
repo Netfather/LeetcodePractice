9c07cfc6892d4eca487dc15be18b83b72c3fe6a7
Netfather <ahhssxlcwjz@163.com>
2021-03-10 16:10:15 +0900

    一维前缀和 很简单

--- AcWing/AcWing_Winter_795.cpp
+++ AcWing/AcWing_Winter_795.cpp
@@ -18,8 +18,30 @@
 #include <climits>
 using namespace std;
 
+const int N = 100010;
+int w[N];  //表示存储的原数组
+int s[N];  //表示从1开始加到第i个元素 （包括第i个 的和
+
 int main()
 {
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) cin >> w[i];
+
+ //处理前缀和
+	for (int i = 1; i <= n; i++)
+	{
+		s[i] = w[i]+ s[i - 1];
+	}
+	while (m--)
+	{
+		int l, r;
+		cin >> l >> r;
+		cout << s[r] - s[l - 1] << endl;
+
+	}
+
 
 	return 0;
 }
