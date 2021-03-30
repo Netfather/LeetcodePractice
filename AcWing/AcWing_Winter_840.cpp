5de4dc3f4056c332f98aa40f08a9e2fd24b9a43c
Netfather <ahhssxlcwjz@163.com>
2021-03-30 14:18:07 +0900

    哈希表，这道题本意是模拟一下哈希表，但是最有用的还是字符串哈希

---
+++ AcWing/AcWing_Winter_840.cpp
@@ -0,0 +1,42 @@
+//Ë¼Â·£º
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+
+int main()
+{
+	unordered_set<int> res;
+	int op;
+	cin >> op;
+	while (op--)
+	{
+		char Op;
+		int a;
+		cin >> Op >> a;
+		if (Op == 'I') res.insert(a);
+		else
+		{
+			if (res.count(a)) cout << "Yes" << endl;
+			else cout << "No" << endl;
+		}
+
+
+	}
+	return 0;
+}
