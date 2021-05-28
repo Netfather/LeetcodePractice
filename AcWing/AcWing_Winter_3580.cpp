6e3a25778ce842ffdef43929798ef6447cf5a3f4
Netfather <ahhssxlcwjz@163.com>
2021-05-28 14:53:47 +0900

    排序算法的变体，使用反证法证明一下即可

---
+++ AcWing/AcWing_Winter_3580.cpp
@@ -0,0 +1,38 @@
+//Ë¼Â·£º
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+const int N = 100010;
+int w[N];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> w[i];
+	sort(w, w + n);
+	int res = 0;
+	for (int i = n - 1; i > 0;)
+	{
+		if (w[i] != w[i - 1]) res += (w[i] - w[i - 1]);
+		i = i - 2;
+	}
+	cout << res << endl;
+
+	return 0;
+}
