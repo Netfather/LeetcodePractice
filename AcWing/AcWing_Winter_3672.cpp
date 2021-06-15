b036af5d8a57d79b0e7a238181ec2353f90c0d5b
Netfather <ahhssxlcwjz@163.com>
2021-06-15 13:36:02 +0900

    数学推导

---
+++ AcWing/AcWing_Winter_3672.cpp
@@ -0,0 +1,43 @@
+//Ë¼Â·£º
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+
+const int N = 110;
+int w[N];
+
+bool cmp(int x, int y) {
+	return x > y;
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) cin >> w[i];
+		sort(w, w + n,cmp);
+		for (int i = 0; i < n; i++) cout << w[i] <<" ";
+		cout << endl;
+	}
+	return 0;
+}
