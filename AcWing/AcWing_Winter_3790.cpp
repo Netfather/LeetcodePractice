6e6966da0547a0b02b32548b60ac67748d2de432
Netfather <ahhssxlcwjz@163.com>
2021-08-03 17:26:40 +0900

    录入单词，非常简单的问题。模拟题就行

---
+++ AcWing/AcWing_Winter_3790.cpp
@@ -0,0 +1,44 @@
+//思路：
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+//非常简单的模拟题   预处理一下 长度为n的数组 将每个单词之间的时差录入
+// 考察新数组   最靠近最后位置的   >c 位置的 数字
+
+int res = 1; 
+const int N = 100010;
+int t[N];
+int b_t[N];
+
+int main()
+{
+	int n, c;
+	cin >> n >> c;
+
+	for (int i = 1; i <= n; i++) cin >> t[i];
+	for (int i = 1; i < n; i++) b_t[i] = t[i + 1] - t[i];
+
+	for (int i = n - 1; i >= 1; i--)
+	{
+		if (b_t[i] <= c) res++;
+		else break;
+	}
+	cout << res << endl;
+	return 0;
+}
