8621a196c960c48512de95b6b633b1a928d04f39
Netfather <ahhssxlcwjz@163.com>
2021-08-12 16:22:51 +0900

    奇数还是偶数，考察奇偶相加相乘之间的性质转换，非常简单

---
+++ AcWing/AcWing_Winter_3800.cpp
@@ -0,0 +1,57 @@
+//思路：
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+//首先明确  奇数乘奇数 必定为奇数   其余的必定为偶数
+// 然后  偶数+偶数  为偶数   奇数+奇数为偶数  奇数+偶数为奇数  这是一个异或操作
+// 因此这道题就是再度如ak的时候  就判断出该项是奇数还是偶数   然后把对应的数字做一个异或
+//时间复杂度为 10的6次方 可以过
+const int N = 100010;
+int a[N];
+
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int b, k;
+		cin >> b >> k;
+		for (int i = 1; i <= k; i++) cin >> a[i];
+		for (int i = 1; i < k; i++)
+		{
+			if (b % 2 == 1 && a[i] % 2 == 1) a[i] = 0;
+			else a[i] = 1;
+		}
+		//处理一下尾巴
+		if (a[k] % 2 == 1) a[k] = 0;
+		else a[k] = 1;
+
+		int res = a[1];
+		for (int i = 2; i <= k; i++)
+		{
+			res = !(res ^ a[i]);
+		}
+		if (res == 0) cout << "odd" << endl;
+		else cout << "even " << endl;
+
+	}
+	return 0;
+}
